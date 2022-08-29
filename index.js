555909babfc4809497c61a9dd66b8c1592f347a7
matebog95 <112423504+matebog95@users.noreply.github.com>
2022-08-29 21:04:32 +0200

    JS
    JS step 2

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-// 1. Create two variables, firstCard and secondCard. 
-
 let firstCard = Math.floor((Math.random()*11)+2);
 let secondCard = Math.floor((Math.random()*11)+2);
+let thirdCard = Math.floor((Math.random()*11)+2);
 
 // Set their values to a random number between 2-11
-console.log(firstCard);
 // 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard;
+
+if( sum < 21){
+    
+    console.log("Do you want drew a new card?")
+    sum =+ thirdCard;
+
+} else if ( sum === 21)
+{
+    console.log("You WIN!!!")
+    
+
+}
+else{
+        console.log("You LOST!!!")
+}
+
+console.log(sum);
