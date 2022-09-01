6f9397718fe1d4e0296cc829fcd15b384209ab4d
matebog95 <112423504+matebog95@users.noreply.github.com>
2022-09-01 21:30:19 +0200

    NEW Ver
    appload

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-let firstCard = Math.floor((Math.random()*11)+2);
-let secondCard = Math.floor((Math.random()*11)+2);
-let newCard = Math.floor((Math.random()*11)+2);
-let newCard2 = Math.floor((Math.random()*11)+2);
+let firstCard;
+let secondCard;
+let newCard; 
+let newCard2;
 let infoID = document.getElementById("info");
 let CardsID = document.getElementById("Cards");
 let sumID = document.getElementById("Suma");
-
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function handleClick() {
-  btn.innerHTML = 'New Game';
-});
-
-let sum = firstCard + secondCard;
+let sum;
 
 function startGameButton(){
 
+    firstCard = Math.floor((Math.random()*11)+2);
+    secondCard = Math.floor((Math.random()*11)+2);
+    sum = firstCard + secondCard;
     CardsID.innerHTML= "Cards: " + firstCard + " " + secondCard;
     sumID.innerHTML = "Sum: "+ sum;
    
     if( sum < 21){
 
         infoID.textContent="Do you want drew a new card?";
+     
     }
     
     else if ( sum === 21)
     {
         infoID.textContent="You WIN!!!";
+     
     }
     else{
         infoID.textContent="You LOST!!!";
+    
     
     } 
 }
 function DrewCardButton(){
 
-    CardsID.innerHTML= "Cards: " + firstCard + " " + secondCard + " " + newCard
+  
+    newCard = Math.floor((Math.random()*11)+2);
+    CardsID.innerHTML= "Cards: " + firstCard + " " + secondCard + " " + newCard 
     sum = sum + newCard;
-    
     sumID.innerHTML = "Sum: "+ sum ;
-   
+
     if( sum < 21){
 
         infoID.textContent="Do you want drew a new card?";
         
      }
-    
     else if ( sum === 21)
     {
         infoID.textContent="You WIN!!!";
@@ -54,5 +53,4 @@ function DrewCardButton(){
         infoID.textContent="You LOST!!!";
     
     } 
-}
-
+    }
